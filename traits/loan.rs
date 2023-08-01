1387577ae87d4c84c15d564aa1b1ea76bdb57b1d
Recrafter <renehurter0@gmail.com>
2023-08-01 21:01:02 +0200

    adding a loan error

--- traits/loan.rs
+++ traits/loan.rs
@@ -47,5 +47,6 @@ impl Loan: ownable {
 #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 pub enum LoanError {
-
+    /// This error will be thrown if there is no loan for the loan_id,
+    InvalidLoanId,
 }
