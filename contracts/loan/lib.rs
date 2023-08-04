a2c48a72ad9ce4aa3a07b42e732330e878e840e8
Recrafter <renehurter0@gmail.com>
2023-08-04 22:03:53 +0200

    adding the first version of the delete_loan and withdraw function

--- contracts/loan/lib.rs
+++ contracts/loan/lib.rs
@@ -1,144 +1,110 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-#[openbrush::implementation(PSP34, Ownable, PSP34Metadata)]
-#[ink::contract]
+#[openbrush::contract]
 pub mod loan {
 
-    use LendingProtocol_Contracts::traits::loan::*;
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    use xcavate_lending_protocol::traits::loan::*;
+
+    use openbrush::{
+        modifiers,
+        storage::Mapping,
+        traits::{
+            DefaultEnv,
+            Storage,
+            String
+        },
+    };
+
+    use ink::env::{
+        call::{build_call, ExecutionInput, Selector},
+        DefaultEnvironment,
+    };
+
     #[ink(storage)]
-    pub struct Loan {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    //#[derive(Default, Storage)]
+    pub struct LoanContract {
+        borrower: AccountId,
+        collateral_nft: AccountId,
+        collateral_price: Balance,
+        /// amount of token that the lender took from the contract
+        borrow_amount: Balance,
+        liquidation_price: Balance,
+        timestamp: Timestamp,
+        liquidated: bool,
     }
 
-    impl Loan {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
+    impl Loan for LoanContract {
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        fn delete_loan(&mut self){
+            <Self as DefaultEnv>::env().terminate_contract(<Self as DefaultEnv>::env().caller());
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let loan = Loan::default();
-            assert_eq!(loan.get(), false);
+        fn update_loan(&mut self, new_borrow_amount: Balance, new_timestamp: Timestamp) -> Result<(), LoanError>
+        {
+            
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut loan = Loan::new(false);
-            assert_eq!(loan.get(), false);
-            loan.flip();
-            assert_eq!(loan.get(), true);
-        }
-    }
 
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = LoanRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("loan", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<LoanRef>(contract_account_id.clone())
-                .call(|loan| loan.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
+        #[ink(message)]
+        fn liquidate_loan(&mut self) -> Result<(), LoanError>
+        {
+            if 2 > 3 {
+                return Err(LoanError::NotTheBorrower)
+            }
             Ok(())
         }
 
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = LoanRef::new(false);
-            let contract_account_id = client
-                .instantiate("loan", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
 
-            let get = build_message::<LoanRef>(contract_account_id.clone())
-                .call(|loan| loan.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<LoanRef>(contract_account_id.clone())
-                .call(|loan| loan.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<LoanRef>(contract_account_id.clone())
-                .call(|loan| loan.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
+        #[ink(message)]
+        fn repay(&mut self, repay_amount: Balance) -> Result<(), LoanError>
+        {
+            if 2 > 3 {
+                return Err(LoanError::NotTheBorrower)
+            }
             Ok(())
         }
+    
+
+        #[ink(message)]
+        fn withdraw_funds(&mut self, amount: Balance) -> Result<(), LoanError> {
+            if amount <= <Self as DefaultEnv>::env().balance() {
+                return Err(LoanError::InsufficientLoanBalance)
+            }
+            if self.borrower != <Self as DefaultEnv>::env().caller() {
+                return Err(LoanError::NotTheBorrower)
+            }
+            <Self as DefaultEnv>::env().transfer(self.borrower, amount);
+            self.borrow_amount += amount;
+            Ok(())
+        }
+
+
+        
+    }
+
+    impl LoanContract {
+        /// Constructor that initializes loan information for the contract
+        #[ink(constructor, payable)]
+        pub fn new(borrower: AccountId, collateral_nft: AccountId, collateral_price: Balance, borrow_amount: AccountId, liquidation_price: Balance) -> Self {
+            let timestamp = <Self as DefaultEnv>::env().block_timestamp();
+            let liquidated = Default::default();
+            let borrow_amount = 0;
+            
+            LoanContract {
+                borrower,
+                collateral_nft,
+                collateral_price,
+                borrow_amount,
+                liquidation_price,
+                timestamp,
+                liquidated
+            }
+        }
+/*        #[ink(message)]
+        pub fn get_loan_info(&self) -> LoanContract {
+            self
+        }  */
     }
 }
